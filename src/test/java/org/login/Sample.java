8956318eefd3f21f98e56be8a4412c3a32631759
HemalathaVell <hemavel4440@gmail.com>
2024-07-02 19:34:46 +0530

    firstcommit

---
+++ src/test/java/org/login/Sample.java
@@ -0,0 +1,32 @@
+package org.login;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Sample {
+	public static void main(String[] args) throws IOException {
+		FileReader fileReader = new FileReader(new File("C:\\Users\\hemav\\eclipse-workspace\\Deserialization\\src\\test\\resources\\Sample.json"));
+		ObjectMapper objectMapper = new ObjectMapper();
+		Root readValue = objectMapper.readValue(fileReader, Root.class);
+		System.out.println(readValue.getName());
+		System.out.println(readValue.getAge());
+		System.out.println(readValue.isStatus());
+		
+		Address address = readValue.getAddress();
+		System.out.println(address.getCity());
+		System.out.println(address.getState());
+		
+		ArrayList<String> courses = readValue.getCourses();
+		for (int i = 0; i < courses.size(); i++) {
+			String string = courses.get(i);
+			System.out.println(string);
+			
+		}
+	}
+
+}
